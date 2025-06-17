d318c6d9379ccf9933a9900e6bcddb9de3a968bb
chandan <chandansahoo02468@gmail.com>
2025-06-17 17:29:01 +0530

    Add tiptap extensions

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,14 +1,15 @@
+import { Editor } from "./Editor";
+
 interface DocumentIdPageProps {
     params : Promise<{documentId: string}>;
 }
 
 
 const DocumentIdPage = async ({params}: DocumentIdPageProps) => {
-    const awaitedParams = await params;
-    const documentId = awaitedParams.documentId
+    const {documentId} = await params;
     return (
-        <div>
-            Document ID Page with documentId: {documentId}
+        <div className="min-h-screen bg-[#FAFBFD]">
+            <Editor/>
         </div>
     );
 }
